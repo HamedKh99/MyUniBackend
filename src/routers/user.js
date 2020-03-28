a94d737293746381c25d93824531aa7730648252
HamedKh99 <goo.hamedacc@gmail.com>
2020-03-29 03:33:02 +0430

    added logout and auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
-
+const auth = require('../middleware/auth')
 
 const router = express.Router()
 
@@ -8,7 +8,8 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-        res.status(201).send({user})
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch(e) {
         res.status(400).send(e)
     }
@@ -21,6 +22,16 @@ router.post('/users/login' , async (req, res) => {
         res.send({user, token})
     } catch (e) {
         res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
